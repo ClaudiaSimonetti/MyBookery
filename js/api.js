057244d790136bad3e7db62eece83a285da37b24
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2023-10-13 03:51:54 +0000

    agregue vue a los fetch pero aun falta el modal

--- js/api.js
+++ js/api.js
@@ -1,134 +1,152 @@
+// new Vue({
+//     el: '#app',
+//     data: {
+//       searchTerm: '',
+//       searchResults: [],
+//       mysteryBooks: [],
+//       cookingBooks: [],
+//     },
+//     methods: {
+//       searchBooks() {
+//         var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.searchTerm);
+
+//         fetch(apiUrl)
+//           .then(response => response.json())
+//           .then(data => {
+//             this.searchResults = data.items;
+//             // También actualiza los resultados de misterio y cocina con los resultados generales
+//             this.mysteryBooks = data.items;
+//             this.cookingBooks = data.items;
+//           })
+//           .catch(error => console.error('Error:', error));
+//       },
+//       loadMysteryBooks() {
+//         var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:mystery&maxResults=5';
+
+//         fetch(apiUrl)
+//           .then(response => response.json())
+//           .then(data => {
+//             this.mysteryBooks = data.items;
+//           })
+//           .catch(error => console.error('Error:', error));
+//       },
+//       loadCookingBooks() {
+//         var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:cooking&maxResults=5';
+
+//         fetch(apiUrl)
+//           .then(response => response.json())
+//           .then(data => {
+//             this.cookingBooks = data.items;
+//           })
+//           .catch(error => console.error('Error:', error));
+//       },
+//     },
+//     mounted() {
+//       // Llama a las funciones para cargar los libros al cargar la página
+//       this.loadMysteryBooks();
+//       this.loadCookingBooks();
+//     },
+//   });
+
+// new Vue({
+//     el: '#app',
+//     data: {
+//         searchTerm: '',
+//         searchResults: [],
+//         mysteryBooks: [],
+//         cookingBooks: [],
+//     },
+//     methods: {
+//         testClick() {
+//             console.log('Button clicked!');
+//           },
+//         searchBooks() {
+//             console.log('searchBooks clicked!');
+//         },
+//         loadMysteryBooks() {
+//             var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:mystery&maxResults=5';
+
+//             fetch(apiUrl)
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     this.mysteryBooks = data.items;
+//                 })
+//                 .catch(error => console.error('Error:', error));
+//         },
+//         loadCookingBooks() {
+//             var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:cooking&maxResults=5';
+
+//             fetch(apiUrl)
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     this.cookingBooks = data.items;
+//                 })
+//                 .catch(error => console.error('Error:', error));
+//         },
+//     },
+//     mounted() {
+//         this.loadMysteryBooks();
+//         this.loadCookingBooks();
+//     },
+// });
+
+
+new Vue({
+    el: '#app',
+    data: {
+        searchTerm: '',
+        searchResults: [],
+        mysteryBooks: [],
+        cookingBooks: [],
+    },
+    methods: {
+        
+        searchBooks: function () {
+            console.log('searchBooks inside Vue called!');
+            var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.searchTerm);
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    this.searchResults = data.items;
+                })
+                .catch(error => console.error('Error:', error));
+        },
+        loadMysteryBooks() {
+            var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:mystery';
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    this.mysteryBooks = data.items;
+                })
+                .catch(error => console.error('Error:', error));
+        },
+        loadCookingBooks() {
+            var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:cooking';
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    this.cookingBooks = data.items;
+                })
+                .catch(error => console.error('Error:', error));
+        },
+    },
+    mounted() {
+        this.loadMysteryBooks();
+        this.loadCookingBooks();
+    },
+});
+
 function searchBooks() {
-    // Obtiene el valor del campo de búsqueda
-    var searchTerm = document.getElementById('searchInput').value;
+    console.log('searchBooks outside Vue called!');
+    var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(this.searchTerm);
 
-    // Construye la URL de la API de Google Books
-    var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(searchTerm);
-
-    // Realiza la solicitud a la API utilizando fetch
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Maneja los datos de la respuesta
-            displayResults(data.items);
+            this.searchResults = data.items;
         })
         .catch(error => console.error('Error:', error));
 }
-
-function displayResults(books) {
-    // Limpia los resultados anteriores
-    var resultsContainer = document.getElementById('results');
-    resultsContainer.innerHTML = '';
-
-    // Muestra cada libro en los resultados
-    books.forEach(book => {
-        var title = book.volumeInfo.title;
-        var authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Desconocido';
-        var image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/150'; // Imagen de relleno si no hay imagen disponible
-
-        // Crea un elemento de resultado
-        var resultItem = document.createElement('div');
-        resultItem.className = 'carta';
-        resultItem.innerHTML = `
-            <img src="${image}" alt="Imagen del libro ${title}">
-            <h2>${title}</h2>
-            <p>Autores: ${authors}</p>
-            <button class="btn-modal" data-target="modal1">Ver Detalles</button>
-        `;
-
-        // Agrega el resultado al contenedor
-        resultsContainer.appendChild(resultItem);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Llama a la función para cargar las cartas de misterio al cargar la página
-    loadMysteryBooks();
-});
-
-function loadMysteryBooks() {
-    // Construye la URL de la API de Google Books para libros de misterio
-    var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:mystery&maxResults=5';
-
-    // Realiza la solicitud a la API utilizando fetch
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Maneja los datos de la respuesta y muestra las cartas en la página
-            displayMysteryResults(data.items);
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function displayMysteryResults(books) {
-    // Obtén el contenedor donde se mostrarán las cartas
-    var resultsContainer = document.getElementById('mysteryBooks');
-
-    // Muestra cada libro en los resultados
-    books.forEach(book => {
-        var title = book.volumeInfo.title;
-        var authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Desconocido';
-        var image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/150';
-
-        // Crea un elemento de resultado
-        var resultItem = document.createElement('div');
-        resultItem.className = 'carta';
-        resultItem.innerHTML = `
-            <img src="${image}" alt="Imagen del libro ${title}">
-            <h2>${title}</h2>
-            <p>Autores: ${authors}</p>
-            <button class="btn-modal" data-target="modal1">Ver Detalles</button>
-        `;
-
-        // Agrega el resultado al contenedor
-        resultsContainer.appendChild(resultItem);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Llama a la función para cargar las cartas de cocina al cargar la página
-    loadCookingBooks();
-});
-
-function loadCookingBooks() {
-    // Construye la URL de la API de Google Books para libros de cocina
-    var apiUrl = 'https://www.googleapis.com/books/v1/volumes?q=subject:cooking&maxResults=5';
-
-    // Realiza la solicitud a la API utilizando fetch
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Maneja los datos de la respuesta y muestra las cartas en la página
-            displayCookingResults(data.items);
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-function displayCookingResults(books) {
-    // Obtén el contenedor donde se mostrarán las cartas de cocina
-    var resultsContainer = document.getElementById('cookingBooks');
-
-    // Muestra cada libro en los resultados
-    books.forEach(book => {
-        var title = book.volumeInfo.title;
-        var authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Desconocido';
-        var image = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/150';
-
-        // Crea un elemento de resultado
-        var resultItem = document.createElement('div');
-        resultItem.className = 'carta';
-        resultItem.innerHTML = `
-            <img src="${image}" alt="Imagen del libro ${title}">
-            <h2>${title}</h2>
-            <p>Autores: ${authors}</p>
-            <button class="btn-modal" data-target="modal1">Ver Detalles</button>
-        `;
-
-        // Agrega el resultado al contenedor
-        resultsContainer.appendChild(resultItem);
-    });
-}
-
-
-
-
